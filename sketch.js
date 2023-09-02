5967da0ed3bdfd0a369cb2fcfc2c75f5b26c1576
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-09-02 14:43:40 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,33 +21,8 @@ function setup(){
     World.add(world,plane);
   
     //criados múltiplos corpos de blocos 
-    var block1_options = {
-      restitution:0.5,
-      friction:0.02,
-      frictionAir:0
-    }
-
-    var block2_options = {
-      restitution:0.7,
-      friction:0.01,
-      frictionAir:0.1
-    }
-
-    var block3_options = {
-      restitution:0.01,
-      friction:1,
-      frictionAir:0.3
-    }
-
-    block1 = Bodies.circle(220,10,10,block1_options);
-    World.add(world,block1);
-    
-    block2 = Bodies.rectangle(110,50,10,10,block2_options);
-    World.add(world,block2);
-    
-    block3 = Bodies.rectangle(350,50,10,10,block3_options);
-    World.add(world,block3);
-    
+      
+  
     
     //estilizando os corpos aqui
     fill("brown");
